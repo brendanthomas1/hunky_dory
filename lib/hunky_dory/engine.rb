7910da0c17e2634afff72885a731eed195e4bf67
Brendan Thomas <bt331609@gmail.com>
2017-08-16 11:06:03 -0400

    append migrations

--- lib/hunky_dory/engine.rb
+++ lib/hunky_dory/engine.rb
@@ -1,5 +1,13 @@
 module HunkyDory
   class Engine < ::Rails::Engine
     isolate_namespace HunkyDory
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match(root.to_s)
+        config.paths['db/migrate'].expanded.each do |expand_path|
+          app.config.paths['db/migrate'] << expand_path
+        end
+      end
+    end
   end
 end
