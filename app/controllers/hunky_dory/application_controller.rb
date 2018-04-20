4344d3f16ed98c1ead5d352d4712ac7884d33667
Brendan Thomas <bt331609@gmail.com>
2018-04-19 21:06:07 -0400

    update flash messages

--- app/controllers/hunky_dory/application_controller.rb
+++ app/controllers/hunky_dory/application_controller.rb
@@ -1,5 +1,12 @@
 module HunkyDory
   class ApplicationController < Hunky::ApplicationController
     protect_from_forgery with: :exception
+
+    def flash_errors(model_with_errors)
+      messages = model_with_errors.errors.map do |attribute, message|
+        "#{attribute.capitalize} #{message}"
+      end
+      flash[:danger] = messages.join(', ')
+    end
   end
 end
--- app/controllers/hunky_dory/changes_controller.rb
+++ app/controllers/hunky_dory/changes_controller.rb
@@ -4,25 +4,18 @@ module HunkyDory
   class ChangesController < ApplicationController
     before_action :set_change, only: %i[show edit update destroy]
 
-    # GET /changes
     def index
       @changes = Change.all.order(created_at: :desc)
     end
 
-    # GET /changes/1
     def show; end
 
-    # GET /changes/new
     def new
       @change = Change.new
     end
 
-    # GET /changes/1/edit
     def edit; end
 
-    # POST /changes
-    # TODO: Improve Flash messages
-    # rubocop:disable Metrics/MethodLength
     def create
       @change = Change.new(change_params)
 
@@ -30,26 +23,21 @@ module HunkyDory
         flash[:success] = 'Change was successfully created.'
         redirect_to @change
       else
-        messages = @change.errors.map do |attribute, message|
-          "#{attribute.capitalize} #{message}"
-        end
-        flash[:danger] = messages
+        flash_errors @change
         render :new
       end
     end
-    # rubocop:enable Metrics/MethodLength
 
-    # PATCH/PUT /changes/1
     def update
       if @change.update(change_params)
         flash[:success] = 'Change was successfully updated.'
         redirect_to @change
       else
+        flash_errors @change
         render :edit
       end
     end
 
-    # DELETE /changes/1
     def destroy
       @change.destroy
       flash[:success] = 'Change was successfully destroyed.'
@@ -58,12 +46,10 @@ module HunkyDory
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_change
       @change = Change.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def change_params
       params.require(:change)
             .permit(:summary, :before_description, :after_description)
